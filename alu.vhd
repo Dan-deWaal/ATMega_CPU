16951b7e604e89f0d4f21d613805575c93b4a4db
Hunter <ewollypop@gmail.com>
2017-10-03 13:44:45 +1300

    Added opcode.txt and added comments for each code

--- alu.vhd
+++ alu.vhd
@@ -15,6 +15,7 @@ end entity alu;
 architecture alu_arch of alu is
 	signal sum : signed(15 downto 0);
 	signal sub : signed(15 downto 0);
+	signal logic_and : signed(7 downto 0);
 	signal negative : std_logic;
 	signal overflow : std_logic;
 begin
@@ -24,21 +25,27 @@ begin
 	-- opcode SUB == 000011
 	-- opcode 16 bit SUB == 000100
 	sub <= X - ("00000000" & Y);
+	-- opcode AND == 000101
+	
+	
 
+	-- carry
 	with CONTROL(5 downto 0) select
 		STATUS(0) <= (X(7) and Y(7)) or (Y(7) and not sum(7)) or (not sum(7) and X(7)) when "000001",
 					 not sum(15) and X(15) when "000010",
 					 (not X(7) and Y(7)) or (Y(7) and sum(7)) or (sum(7) and not X(7)) when "000011",
 					 sum(15) and not X(15) when "000100",
 					 '-' when others;
-
+					 
+	-- zero
 	with CONTROL(5 downto 0) select
 		STATUS(1) <= not sum(7) and not sum(6) and not sum(5) and not sum(4) and not sum(3) and not sum(2) and not sum(1) and not sum(0) when "000001",
 					 not sum(15) and not sum(14) and not sum(13) and not sum(12) and not sum(11) and not sum(10) and not sum(9) and not sum(8) and not sum(7) and not sum(6) and not sum(5) and not sum(4) and not sum(3) and not sum(2) and not sum(1) and not sum(0) when "000010",
 					 not sum(7) and not sum(6) and not sum(5) and not sum(4) and not sum(3) and not sum(2) and not sum(1) and not sum(0) when "000011",
 					 not sum(15) and not sum(14) and not sum(13) and not sum(12) and not sum(11) and not sum(10) and not sum(9) and not sum(8) and not sum(7) and not sum(6) and not sum(5) and not sum(4) and not sum(3) and not sum(2) and not sum(1) and not sum(0) when "000100",
 					 '-' when others;
-
+					 
+	-- negative
 	with CONTROL(5 downto 0) select
 		negative <= sum(7) when "000001",
 					sum(15) when "000010",
@@ -47,6 +54,7 @@ begin
 					'-' when others;
 	STATUS(2) <= negative;
 
+	-- overflow
 	with CONTROL(5 downto 0) select
 		overflow <= (X(7) and Y(7) and not sum(7)) or (not X(7) and not Y(7) and sum(7)) when "000001",
 					sum(15) and not X(15) when "000010",
@@ -55,18 +63,21 @@ begin
 					'-' when others;
 	STATUS(3) <= overflow;
 
+	-- signed
 	with CONTROL(5 downto 0) select
 		STATUS(4) <= negative xor overflow when "000001",
 					 negative xor overflow when "000010",
 					 negative xor overflow when "000011",
 					 negative xor overflow when "000100",
 					 '-' when others;
-
+					 
+	-- half carry
 	with CONTROL(5 downto 0) select
 		STATUS(5) <= (X(3) and Y(3)) or (Y(3) and not sum(3)) or (not sum(3) and X(3)) when "000001",
 					 (not X(3) and Y(3)) or (Y(3) and sum(3)) or (sum(3) and not X(3)) when "000011",
 					 '-' when others;
-
+					 
+	-- result
 	with CONTROL(5 downto 0) select
 		OUTPUT <= resize(sum, 16) when "000001",
 				  sum when "000010",
