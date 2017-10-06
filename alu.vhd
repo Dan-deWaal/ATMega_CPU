df098a860fe0f25f59fe72954196727fe3974d8d
Jamie Harvey <jharvey@duality.co.nz>
2017-10-06 14:16:04 +1300

    working ALU updated

--- alu.vhd
+++ alu.vhd
@@ -4,7 +4,7 @@ use IEEE.numeric_std.all;
 
 entity alu is
     port (
-        CONTROL: in std_logic_vector(7 downto 0);
+        CONTROL: in std_logic_vector(4 downto 0);
         X: in signed(15 downto 0);
         Y: in signed(7 downto 0);
         OUTPUT: out signed (15 downto 0);
@@ -39,36 +39,57 @@ architecture alu_arch of alu is
 
     signal carry_in : std_logic;
 
+    signal add_result : signed(15 downto 0);
+    signal mul_result : signed(15 downto 0);
+
     signal result : signed(15 downto 0);
+	 signal x_copy : signed(15 downto 0);
     signal y_copy : signed (7 downto 0);
     signal negative : std_logic;
     signal overflow : std_logic;
 begin
-    X(15 downto 8) <= X(15 downto 8) when CONTROL = ADD16_OP or CONTROL = SUB16_OP else
-                      (others <= '0');
+    x_copy(15 downto 8) <= X(15 downto 8) when CONTROL = ADD16_OP or CONTROL = SUB16_OP else
+                      (others => '0');
 
-    X(7 downto 0) <= (others <= '0') when CONTROL = NEG_OP else;
+    x_copy(7 downto 0) <= (others => '0') when CONTROL = NEG_OP else
                      X(7 downto 0);
 
     y_copy <= not Y when CONTROL = SUB_OP or CONTROL = SUB16_OP else
-              not X when CONTROL = NEG_OP else
+              not X(7 downto 0) when CONTROL = NEG_OP else
               Y;
 
     carry_in <= '1' when CONTROL = SUB_OP or CONTROL = SUB16_OP or CONTROL = NEG_OP else
                 '0';
 
+    add_result <= x_copy + ("00000000" & y_copy) + signed(to_stdlogicvector('0' & carry_in));
+    mul_result <= signed(unsigned(X(7 downto 0)) * unsigned(Y)) when CONTROL = MULU_OP or CONTROL = FMULU_OP else
+                  X(7 downto 0) * Y when CONTROL = MULS_OP or CONTROL = FMULS_OP else
+                  resize(X(7 downto 0) * signed('0' & Y(7 downto 0)), 16) when CONTROL = MULSU_OP or CONTROL = FMULSU_OP else
+                  (others => '-');
+
     with CONTROL(4 downto 0) select
-        result(7 downto 0) <= X + ("00000000" & y_copy) + signed("0" & carry_in, 16) when ADD_OP,
-                  X + ("00000000" & y_copy) + signed("0" & carry_in, 16) when ADD16_OP,
-                  X + ("00000000" & y_copy) + signed("0" & carry_in, 16) when SUB_OP,
-                  X + ("00000000" & y_copy) + signed("0" & carry_in, 16) when SUB16_OP,
+        result <= add_result when ADD_OP,
+                  add_result when ADD16_OP,
+                  add_result when SUB_OP,
+                  add_result when SUB16_OP,
                   "00000000" & (X(7 downto 0) and Y) when AND_OP,
                   "00000000" & (X(7 downto 0) or Y) when OR_OP,
                   "00000000" & (X(7 downto 0) xor Y) when XOR_OP,
                   "00000000" & (not X(7 downto 0)) when NOT_OP,
-                  X + ("00000000" & y_copy) + signed("0" & carry_in, 16) when NEG_OP,
-                  unsigned(X(7 downto 0), 8) * unsigned(Y, 8) when MULU_OP,
-                  X(7 downto 0) * Y, 8 when MULS_OP,
-                  X(7 downto 0) * Y, 8 when MULS_OP,
-
+                  add_result when NEG_OP,
+                  mul_result when MULU_OP,
+                  mul_result when MULS_OP,
+                  mul_result when MULSU_OP,
+                  mul_result sll 1 when FMULU_OP,
+                  mul_result sll 1 when FMULS_OP,
+                  mul_result sll 1 when FMULSU_OP,
+                  "00000000" & (X(7 downto 0) sll 1) when LSL_OP, 
+                  "00000000" & (X(7 downto 0) srl 1) when LSR_OP,
+                  "00000000" & shift_right(X(7 downto 0), 1) when ASR_OP,
+                  -- "00000000" & (X(7 downto 0) sll 1) when ROTL_OP, 
+                  -- "00000000" & (X(7 downto 0) sll 1) when ROTR_OP, 
+                  (others => '0') when others;
+	
+	OUTPUT <= result;
+	STATUS <= (others => '0');
 end architecture alu_arch;
